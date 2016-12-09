0b0771ea3125a97b04024cf702aaf72b9393719d
Mounir Khanouri <m.khanouri@live.fr>
2016-12-09 16:35:12 +0100

    feat: hello world endpoint at GET / (#1)

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"github.com/MounirOnGithub/go-rest-service/utils"
+	"github.com/Sirupsen/logrus"
+	"net/http"
+)
 
+func main() {
+	r := utils.NewRouter()
+	err := http.ListenAndServe(":8080", r)
+
+	if err != nil {
+		logrus.Error(err)
+	}
 }
